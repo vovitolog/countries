a8ebbec1ed3deb61a8c02f398d4d440567320ad1
vovitolog <vovitolog@gmail.com>
2023-07-07 18:40:36 +0300

    feat: add pages and ui components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import logo from './logo.svg'
+import {Header} from "./components/Header";
+import {Main} from "./components/Main";
+import {Route, Routes} from "react-router-dom";
+import {HomePage} from "./pages/HomePage";
+import {NotFound} from "./pages/NotFound";
+import {Details} from "./pages/Details";
 
 function App() {
-  return <div className="App">test</div>
+  return (
+      <>
+        <Header />
+        <Main>
+          <Routes>
+            <Route exact path="/" element={
+              <HomePage />
+            } />
+            <Route path="/country/:name" element={<Details />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Main>
+      </>
+  );
 }
-
 export default App
